60324561bcf667827205bbb6f60d7534e6b48493
MatthieuMarchand <matthieu.marchand@mmibordeaux.com>
2023-06-02 15:45:39 +0200

    first commit

---
+++ src/todos/todos.controller.ts
@@ -0,0 +1,30 @@
+import { Controller, Get, Post, Body, Param, Patch } from '@nestjs/common';
+import { TodosService } from './todos.service';
+import { Todo } from './interfaces/todo.interface';
+import { CreateTodoDto } from './dto/create-todo.dto';
+
+@Controller('todos')
+export class TodosController {
+    constructor (private readonly todosService: TodosService ) {}
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        console.log('id', id);
+        return this.todosService.findOne(id);
+    }
+
+    @Get()
+    findAll(): Todo[] {
+        return this.todosService.findAll();
+    }
+
+    @Post()
+    createTodo(@Body() newTodo: CreateTodoDto) {
+        this.todosService.create(newTodo);
+    }
+
+    @Patch(':id')
+    updateTodo(@Param('id') id: string, @Body() todo: CreateTodoDto) {
+        return this.todosService.update(id, todo);
+    }
+}
---
+++ src/todos/todos.service.ts
@@ -0,0 +1,58 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Todo } from './interfaces/todo.interface';
+import { CreateTodoDto } from './dto/create-todo.dto';
+
+@Injectable()
+export class TodosService {
+    todos: Todo[] = [
+    {
+        id: 1,
+        title: 'todos app',
+        description: 'Create NestJS todos app',
+        done: false,
+    },
+    {
+        id: 2,
+        title: 'bread',
+        description: 'buy bread',
+        done: true,
+    },
+    {
+        id: 3,
+        title: 'wine',
+        description: 'buy wine',
+        done: true,
+    },
+    ];
+
+    findOne(id: string) {
+        return this.todos.find(todo => todo.id === Number(id));
+    }
+
+    findAll(): Todo[] {
+        return this.todos;
+    }
+
+    create(todo: CreateTodoDto) {
+        this.todos = [...this.todos, todo];
+    }
+
+    update(id: string, todo: Todo) {
+        const todoToUpdate = this.todos.find(todo => todo.id === +id); // +id = Number(id)
+        if (!todoToUpdate) {
+            return new NotFoundException('todo not found');
+        }
+        if (todo.title) {
+            todoToUpdate.title = todo.title;
+        }
+        if (todo.description) {
+            todoToUpdate.description = todo.description;
+        }
+        if (todo.hasOwnProperty('done')) {
+            todoToUpdate.done = todo.done;
+        }
+        const updatedTodos = this.todos.map(todo => todo.id !== +id ? todo : todoToUpdate)
+        this.todos = [...updatedTodos];
+        return { updatedTodo: 1, todo: todoToUpdate};
+    }
+}
